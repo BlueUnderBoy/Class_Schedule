ae4b59ba99c6d0c66095561c410a1340c55698b9
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-07-13 03:39:20 +0000

    Big Update

--- welcome.rb
+++ welcome.rb
@@ -1,10 +1,15 @@
 require "date"
 class Student
   attr_accessor :first_name, :last_name, :school, :grade
+  attr_writer :date
   
   def initialize(first_name, last_name)
       @first_name = first_name
       @last_name = last_name
+  end
+
+  def date
+    return Date.today
   end
 
   def school=(value)
@@ -25,45 +30,84 @@ end
 class Elementary
   attr_reader :kindergarten, :one, :two, :three, :four, :five
   
-  def initialize(kindergarten, one, two, three, four, five)
-    @kindergarten = "Reading, Dance, Music, Counting, Recess, Garden"
-    @one = "Reading, Writing, Addition & Subtraction, Recess, Garden, Music"
-    @two = "Reading, Writing, Multiplication & Division, Recess, Garden, Music"
-    @three = "Reading, Writing, Multiplication & Division, Recess, Garden, Music"
-    @four = "Reading, Writing, Multiplication & Division, Recess, Life Science, Music"
-    @five = "Reading, Writing, Decimals & Fractions, Recess, Life Science, Music"
+  def kindergarten
+    return "Reading, Dance, Music, Counting, Recess, Garden"
+  end
+
+  def one 
+    return "Reading, Writing, Addition & Subtraction, Recess, Garden, Music"
+  end
+
+  def two
+    return "Reading, Writing, Multiplication & Division, Recess, Garden, Music"
+  end
+
+  def three
+    return "Reading, Writing, Multiplication & Division, Recess, Garden, Music"
+  end
+
+  def four
+    return "Reading, Writing, Multiplication & Division, Recess, Life Science, Music"
+  end
+
+  def five 
+    return "Reading, Writing, Decimals & Fractions, Recess, Life Science, Music"
   end
 end 
 
 class Grammar
   attr_reader :six, :seven, :eight
 
-  def initialize(six, seven, eight)
-    @six = "Language Arts, History, Math 101, PE, Earth Systems"
-    @seven = "Langauge Arts, History, Math 202, PE, Biology"
-    @eight = "Langauge Arts, History, Pre-Algebra, PE, Forces of Earth"
+  def six
+    return "Language Arts, History, Math 101, PE, Earth Systems"
+  end
+  
+  def seven
+    return "Langauge Arts, History, Math 202, PE, Biology"
+  end 
+  
+  def eight
+    return "Langauge Arts, History, Pre-Algebra, PE, Forces of Earth"
   end
 end
 
 class HighSchool
   attr_reader :nine, :ten, :eleven, :twelve
 
-  def initialize(nine, ten, eleven, twelve)
-    @nine = "English, American History 1, Algebra, PE, Biology"
-    @ten = "English, American History 2, Geometry, PE, Physics"
-    @eleven = "Advanced Literature, World History 1, Trigonometry, PE, Chemistry"
-    @twelve = "Advanced Literature, World History 2, Pre-Calculus PE, Molecular Biology"
+  def nine
+    return "English, American History 1, Algebra, PE, Biology"
+  end 
+  
+  def ten
+    return "English, American History 2, Geometry, PE, Physics"
+  end
+  
+  def eleven
+    return "Advanced Literature, World History 1, Trigonometry, PE, Chemistry"
+  end 
+  
+  def twelve
+    return "Advanced Literature, World History 2, Pre-Calculus PE, Molecular Biology"
   end
 end
 
 class College
   attr_reader :freshman, :sophomore, :junior, :senior
 
-  def initialize(freshman, sophomore, junior, senior)
-    @freshman = "English, Ancient History, Philosophy, Calculus 1, Chemistry"
-    @sophomore = "English, Medieval History, Political Science, Calculus 2, Physics"
-    @junior = "English, World Governments, Law 101, Calculus 3, Anatomy"
-    @senior = "Research, Globilization, Law 102, Differential Equations, Quantum Physics"
+  def freshman
+    return "English, Ancient History, Philosophy, Calculus 1, Chemistry"
+  end
+  
+  def sophomore
+    return "English, Medieval History, Political Science, Calculus 2, Physics"
+  end
+  
+  def junior
+    return "English, World Governments, Law 101, Calculus 3, Anatomy"
+  end 
+
+  def senior
+    return "Research, Globilization, Law 102, Differential Equations, Quantum Physics"
   end
 end
 
@@ -97,13 +141,29 @@ unless s == "elementary school" || s == "grammar school" || s == "high school" |
 end
 student.school = s
 
+if s == "elementary school"
   puts "Which grade are you in?"
   g = gets.chomp
-  unless g == "kindergarten"|| g == 1 || g == 2 || g == 3 || g == 4 || g == 5
+  unless g == "kindergarten" || g == 1 || g == 2 || g == 3 || g == 4 || g == 5
     puts "Please enter kindergarten or any number from 1-5..."
     g = gets.chomp
   end
   student.grade = g
+  elementary = Elementary.new
+  puts "Now to reveal your coursework for the upcoming year!"
+  if g.to_s == "kindergarten"
+    puts elementary.kindergarten
+  elsif g.to_s == "1"
+    puts elementary.one
+  elsif g.to_s == "2"
+    puts elementary.two
+  elsif g.to_s == "3"
+    puts elementary.three
+  elsif g.to_s == "4"
+    puts elementary.four
+  else
+    puts elementary.five
+  end 
 elsif s == "grammar school"
   puts "Which grade are you in?"
   g = gets.chomp
@@ -112,6 +172,15 @@ elsif s == "grammar school"
     g = gets.chomp
   end
   student.grade = g
+  grammar = Grammar.new
+  puts "Now to reveal your coursework for the upcoming year!"
+  if g.to_s == "6"
+    puts grammar.six
+  elsif g.to_s == "7"
+    puts grammar.seven
+  else
+    puts grammar.eight
+  end
 elsif s == "high school"
   puts "Which grade are you in?"
   g = gets.chomp
@@ -120,17 +189,37 @@ elsif s == "high school"
     g = gets.chomp
   end
   student.grade = g
+  highschool = HighSchool.new
+  puts "Now to reveal your coursework for the upcoming year!"
+  if g.to_s == "9"
+    puts highschool.nine
+  elsif g.to_s == "10"
+    puts highschool.ten
+  elsif g.to_s == "11"
+    puts highschool.eleven
+  else
+    puts highschool.twelve
+  end
 elsif s == "college"
   puts "Which class are you in?"
   g = gets.chomp.downcase!
   unless g == "freshman" || g == "sophomore" || g == "junior" || g == "senior"
-    puts "Please enter a grade from 9-12..."
+    puts "Please enter either freshman, sophomore, junior, or senior..."
     g = gets.chomp
   end
   student.grade = g
+  college = College.new
+  puts "Now to reveal your coursework for the upcoming year!"
+  if g == "senior"
+    puts college.senior
+  elsif g == "junior"
+    puts college.junior
+  elsif g == "sophomore"
+    puts college.sophomore
+  else
+    puts college.freshman
+  end
 else
 end
 
-
-
-puts "Now to reveal your coursework for the upcoming year!"
+puts "Now save your schedule and prepare for the upcoming year!"
